5189173f5c597e098833ace0f2a49376ac6fb54a
mengbingye <bingyemeng@gmail.com>
2019-03-18 16:49:27 +0800

    feat: add generator

---
+++ src/main/java/com/slabs/springbootjava/core/constant/ProjectConstant.java
@@ -0,0 +1,30 @@
+package com.slabs.springbootjava.core.constant;
+
+/**
+ * @create: 2019/03/18 16:43
+ */
+
+public class ProjectConstant {
+
+    // 项目基础包名称
+    public static final String BASE_PACKAGE = "com.slabs.springbootjava";
+
+    // Model所在包
+    public static final String MODEL_PACKAGE = BASE_PACKAGE + ".model";
+
+    // Mapper所在包
+    public static final String MAPPER_PACKAGE = BASE_PACKAGE + ".dao";
+
+    // Service所在包
+    public static final String SERVICE_PACKAGE = BASE_PACKAGE + ".service";
+
+    // ServiceImpl所在包
+    public static final String SERVICE_IMPL_PACKAGE = SERVICE_PACKAGE + ".impl";
+
+    // Controller所在包
+    public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".controller";
+
+    // Mapper插件基础接口的完全限定名
+    public static final String MAPPER_INTERFACE_REFERENCE = BASE_PACKAGE + ".core.universal.Mapper";
+
+}
---
+++ src/main/java/com/slabs/springbootjava/dao/SystemLogMapper.java
@@ -0,0 +1,7 @@
+package com.slabs.springbootjava.dao;
+
+import com.slabs.springbootjava.core.universal.Mapper;
+import com.slabs.springbootjava.model.SystemLog;
+
+public interface SystemLogMapper extends Mapper<SystemLog> {
+}
